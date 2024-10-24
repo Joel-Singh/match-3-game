4d8f171b111c627363c23998bb46e1bfd3dc667b
Joel Singh <joelsingh788@gmail.com>
2024-10-24 14:05:28 -0400

    Spawned in a red cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::{prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(
+            bevy::log::LogPlugin {
+                level: bevy::log::Level::DEBUG,
+                ..default()
+            }
+        ))
+        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                scale: Vec2::new(10.0, 10.0).extend(1.0),
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: Color::linear_rgb(1.0, 0.0, 0.0),
+                ..default()
+            },
+            ..default()
+        },
+    ));
 }
