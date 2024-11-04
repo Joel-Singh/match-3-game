588044f87f17328a7d1b53a458f3701eecb87739
Joel Singh <joelsingh788@gmail.com>
2024-11-04 11:07:38 -0500

    Implement a grid of squares

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use bevy::prelude::*;
 
 #[derive(Resource)]
-struct Board(Vec<Vec<Entity>>);
+struct Board(Vec<Entity>);
 
 #[derive(Component)]
 struct Shape;
+
+const BOARD_POSITION: Transform = Transform::from_xyz(0.0, 0.0, 0.0);
+const BOARD_SIZE: i32 = 10;
+const BOARD_TOTAL_SHAPES: i32 = BOARD_SIZE * BOARD_SIZE;
+
+const SHAPE_SIZE: i32 = 35;
 
 fn main() {
     App::new()
@@ -16,6 +22,7 @@ fn main() {
         ))
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, position_board_elements)
         .insert_resource(Board(Vec::new()))
         .run();
 }
@@ -28,16 +35,33 @@ fn setup(
     mut commands: Commands,
     mut board: ResMut<Board>
 ) {
-    let spawned_shape = commands.spawn((
-        Shape,
-        SpriteBundle {
-            transform: Transform {
-                scale: Vec2::new(10.0, 10.0).extend(1.0),
+    for _ in 0..BOARD_TOTAL_SHAPES {
+        let spawned_shape = commands.spawn((
+            Shape,
+            SpriteBundle {
+                transform: Transform {
+                    scale: Vec2::new(10.0, 10.0).extend(1.0),
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        },
-    ));
-    board.0.push(Vec::new());
-    board.0[0].push(spawned_shape.id());
+        ));
+
+        board.0.push(spawned_shape.id());
+    }
+}
+
+fn position_board_elements(
+    board: ResMut<Board>,
+    mut query: Query<&mut Transform, With<Shape>>
+) {
+    for (index, entity) in board.0.iter().enumerate() {
+        let column: i32 = (index as i32 % 10) + 1;
+        let row: i32 = (index as i32 / 10) + 1;
+
+        let [mut shape_transform] = query.get_many_mut([*entity]).expect("Entity in board exists");
+
+        shape_transform.translation.x = (SHAPE_SIZE * column) as f32;
+        shape_transform.translation.y = (SHAPE_SIZE * row * -1) as f32;
+    }
 }
