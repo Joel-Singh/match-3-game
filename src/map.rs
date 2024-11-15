e4c451a7a32e96f6d8483282d221793dc73d4ee3
Joel Singh <joelsingh788@gmail.com>
2024-11-15 15:56:01 -0500

    Add three buttons

--- src/map.rs
+++ src/map.rs
@@ -5,6 +5,13 @@ use crate::GameState;
 #[derive(Component)]
 pub struct Map;
 
+#[derive(Component)]
+enum Areas {
+    First,
+    Second,
+    Third,
+}
+
 pub fn map(app: &mut App) {
     app
         .add_systems(OnEnter(GameState::Map), setup)
@@ -21,21 +28,29 @@ fn setup(mut commands: Commands) {
             margin: UiRect::all(Val::Auto),
             ..default()
         },
-        background_color: GRAY_50.into(),
         ..default()
     })).with_children(|parent| {
-        parent.spawn(ButtonBundle {
-            style: Style {
-                width: Val::Px(50.),
-                height: Val::Px(50.),
-                margin: UiRect::all(Val::Px(5.)),
-                ..default()
-            },
-            ..default()
-        });
+        parent.spawn(get_area_button(Areas::First));
+        parent.spawn(get_area_button(Areas::Second));
+        parent.spawn(get_area_button(Areas::Third));
     });
 }
 
 fn cleanup(mut commands: Commands, map: Query<Entity, With<Map>>) {
     commands.entity(map.single()).despawn_recursive();
+}
+
+fn get_area_button(area: Areas) -> (ButtonBundle, Areas) {
+    (ButtonBundle {
+        style: Style {
+            width: Val::Px(50.),
+            height: Val::Px(50.),
+            margin: UiRect::all(Val::Px(5.)),
+            ..default()
+        },
+        background_color: GRAY_50.into(),
+        ..default()
+    },
+    area
+    )
 }
