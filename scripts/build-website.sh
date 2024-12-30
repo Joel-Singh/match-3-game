d3f022b5e77305b0f42424ea980f105209db44d1
Joel Singh <joelsingh788@gmail.com>
2024-12-30 14:36:32 -0500

    Move output, dist -> docs for gh-pages

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -1,6 +1,8 @@
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 cargo build --release --target wasm32-unknown-unknown
 
-wasm-bindgen --no-typescript --target web --out-dir ./dist/ --out-name "match-3" ./target/wasm32-unknown-unknown/release/match-3-game.wasm
+mkdir -p docs
 
-cp ./src/index.html ./dist/
+wasm-bindgen --no-typescript --target web --out-dir ./docs/ --out-name "match-3" ./target/wasm32-unknown-unknown/release/match-3-game.wasm
+
+cp ./src/index.html ./docs/
