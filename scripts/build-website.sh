304616c942aaa1e1a8d1ebfb0d3cfa9ea49f06b7
Joel Singh <joelsingh788@gmail.com>
2024-12-31 01:06:50 -0500

    Change build script to automatically checkout reset to main

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -3,6 +3,9 @@ if [[ -n $(git status --porcelain) ]]; then
 	exit 1
 fi
 
+git checkout gh-pages
+git reset --hard main
+
 export RUSTFLAGS="--cfg=web_sys_unstable_apis"
 cargo build --release --target wasm32-unknown-unknown
 
@@ -11,3 +14,9 @@ mkdir -p docs
 wasm-bindgen --no-typescript --target web --out-dir ./docs/ --out-name "match-3" ./target/wasm32-unknown-unknown/release/match-3-game.wasm
 
 cp ./src/index.html ./docs/
+
+git add -A
+git commit -m "Build"
+
+git push --force
+git checkout main
